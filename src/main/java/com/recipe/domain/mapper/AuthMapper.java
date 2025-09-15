f6969a39b7b36c11d432964cf106c32418e8bbbc
t-ootsubo <takashi.tsubo@gmail.com>
2025-09-15 15:15:36 +0900

    change

---
+++ src/main/java/com/recipe/domain/mapper/AuthMapper.java
@@ -0,0 +1,17 @@
+package com.recipe.domain.mapper;
+
+import com.recipe.domain.entity.Auth;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface AuthMapper {
+
+    Auth findByUsername(@Param("username") String username);
+
+    List<String> findRolesByUserId(@Param("userId") Long userId);
+
+    List<String> findPermissionsByUserId(@Param("userId") Long userId);
+}
