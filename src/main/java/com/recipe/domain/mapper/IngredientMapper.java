610eccc3d95c066356c74253e56ab140f8d39638
t-ootsubo <takashi.tsubo@gmail.com>
2025-09-23 00:10:05 +0900

    認可処理まわりを修正

--- src/main/java/com/recipe/domain/mapper/IngredientMapper.java
+++ src/main/java/com/recipe/domain/mapper/IngredientMapper.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface IngredientMapper {
-    
+
     Ingredient findById(Integer ingredientId);
 
     Ingredient findByName(String name);
